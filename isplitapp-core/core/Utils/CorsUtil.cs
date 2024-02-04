48a1fc54ee0fd5df596c54c0129e18d11f764e4d
yet-an-other <main@ivan-b.com>
2024-02-04 12:12:35 +0100

    Add Cors regexps

--- isplitapp-core/core/Utils/CorsUtil.cs
+++ isplitapp-core/core/Utils/CorsUtil.cs
@@ -4,6 +4,6 @@ namespace IB.ISplitApp.Core.Utils;
 
 public static partial class CorsUtil
 {
-    [GeneratedRegex("localhost:\\d{1,5}|isplit.app|dev.isplit.app|apidev.isplit.app|api.isplit.app")]
+    [GeneratedRegex("localhost:\\d{1,5}|isplit.app|dev.isplit.app|apidev.isplit.app|api.isplit.app|192.168.111.36:?\\d{1,5}")]
     public static partial Regex IsValidOrigin();
 }
