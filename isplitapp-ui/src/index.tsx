453439e559f50e4a39f555e04087de1783aa494b
yet-an-other <main@ivan-b.com>
2024-02-13 20:23:03 +0100

    improve sentry integration

--- isplitapp-ui/src/index.tsx
+++ isplitapp-ui/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import CssBaseline from '@mui/material/CssBaseline';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, createRoutesFromChildren, matchRoutes, useLocation, useNavigationType } from 'react-router-dom';
 import Home from './app/Home';
 import HeaderBar from './controls/HeaderBar';
 import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
@@ -15,7 +15,7 @@ import Party from './app/Party';
 import ExpenseList from './app/ExpenseList';
 import Balance from './app/Balance';
 import ExpenseEdit from './app/ExpenseEdit';
-import GlobalError  from './app/GlobalError';
+import { GlobalErrorAlert }  from './app/GlobalError';
 
 import * as Sentry from "@sentry/react";
 import { captureConsoleIntegration, httpClientIntegration } from "@sentry/integrations";
@@ -23,19 +23,27 @@ import { captureConsoleIntegration, httpClientIntegration } from "@sentry/integr
 Sentry.init({
   dsn: "https://8592952298c41c59b935f1f60b154dc8@o4506730525294592.ingest.sentry.io/4506730534600704",
   sendDefaultPii: true,
+  release: `iSplitApp@${process.env.REACT_APP_VERSION}`,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration({
       maskAllText: false,
       blockAllMedia: false,
     }),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),    
     captureConsoleIntegration(),
     httpClientIntegration()
   ],
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", /^https:\/\/isplit\.app/, /^https:\/\/dev\.isplit\.app/],
+  tracePropagationTargets: ["localhost", /^https:\/\/isplit\.app/, /^https:\/\/api\.isplit\.app/, /^https:\/\/dev\.isplit\.app/, /^https:\/\/apidev\.isplit\.app/],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
@@ -56,37 +64,42 @@ let theme = createTheme({
 });
 
 theme = responsiveFontSizes(theme);
+const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
+      <Sentry.ErrorBoundary fallback={<GlobalErrorAlert />}>
       <BrowserRouter>
         <ContentBar>
           <HeaderBar />
           <AlertContextProvider>
-            <GlobalError>
-              <Routes>
-                <Route index element={<Home />} />
-                <Route path="groups/:partyId" element={<Party />} >
-                  <Route path="expenses" element={<ExpenseList />} />
-                  <Route path="expenses/create" element={<ExpenseEdit />} />
-                  <Route path="expenses/:expenseId/edit" element={<ExpenseEdit />} />
-                  <Route path="balance" element={<Balance />} />
-                  <Route index element={<PartyList />} />
-                </Route>
-                <Route path="/groups/create" element={<PartyEdit/>} />
-                <Route path="/groups/:partyId/edit" element={<PartyEdit/>} />
-                <Route path="/groups" element={<PartyList/>} />
-                <Route path="/" element={<Home/>} />
-              </Routes>
-            </GlobalError>
+             <SentryRoutes>
+
+                  <Route index element={<Home />} />
+                  <Route path="groups/:partyId" element={<Party />} >
+                    <Route path="expenses" element={<ExpenseList />} />
+                    <Route path="expenses/create" element={<ExpenseEdit />} />
+                    <Route path="expenses/:expenseId/edit" element={<ExpenseEdit />} />
+                    <Route path="balance" element={<Balance />} />
+                    <Route index element={<ExpenseList />} />
+                  </Route>
+                  <Route path="/groups/create" element={<PartyEdit/>} />
+                  <Route path="/groups/:partyId/edit" element={<PartyEdit/>} />
+                  <Route path="/groups" element={<PartyList/>} />
+                  <Route path="/" element={<Home/>} />
+
+              </SentryRoutes>
           </AlertContextProvider>
         </ContentBar>
         <FooterBar />
-      </BrowserRouter>
+        </BrowserRouter>
+      </Sentry.ErrorBoundary>
     </ThemeProvider>
   </React.StrictMode>
 );
