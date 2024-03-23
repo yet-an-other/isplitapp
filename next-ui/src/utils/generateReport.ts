b2cd56db5bcf5f5ffa1b8ee1e759b1a42147a362
yet-an-other <157240815+yet-an-other@users.noreply.github.com>
2024-03-23 21:27:34 +0100

    fix amount in reporting (#41)

--- next-ui/src/utils/generateReport.ts
+++ next-ui/src/utils/generateReport.ts
@@ -10,21 +10,20 @@ export async function generateReport(partyId: string) {
 
     const particiapntAmount = (participant: ParticipantInfo, expense: ExpenseInfo) => {
         const paidAmount = participant.id === expense.lenderId ? expense.amount : 0;
-        return (paidAmount * 100 - expense.borrowers.filter(b => b.participantId === participant.id)[0]?.amount * 100 || 0) / 100;
+        return (((paidAmount * 100) - (expense.borrowers.filter(b => b.participantId === participant.id)[0]?.amount || 0) * 100) / 100)
+            .toFixed(2);
     }
 
     const csvHeader = `Title${csvDelimiter} Date${csvDelimiter} Amount${party.participants.map(p => `${csvDelimiter} "${p.name}"`).join('')}\n`;
     const csvReport = expenses.map(e  => {
-        const participants = party.participants.map(p => particiapntAmount(p, e).toFixed(2));
+        const participants = party.participants.map(p => particiapntAmount(p, e));
         return `"${e.title}"${csvDelimiter} ${new Date(e.date).toDateString()}${csvDelimiter} ${e.amount}${csvDelimiter} ${participants.join(`${csvDelimiter} `)}\n`;
     })
 
     const csvContent = `data:text/csv;charset=utf-8, ${csvHeader}${csvReport.join('')}`;
-    const encodedURI = encodeURI(csvContent);
-    //window.open(encodedURI);
 
     const link = document.createElement("a");
-    link.href = encodedURI;
-    link.download = `${party.name}_report.csv`;
+    link.href = encodeURI(csvContent);
+    link.download = `${party.name} report.csv`;
     link.click();
 }
