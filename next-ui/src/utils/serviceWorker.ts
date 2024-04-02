5735ff1f9178f23cefbfd666f75589b34e8c90db
yet-an-other <157240815+yet-an-other@users.noreply.github.com>
2024-04-02 02:17:22 +0200

    Fix/swupdate (#50)
    enforce sw update

--- next-ui/src/utils/serviceWorker.ts
+++ next-ui/src/utils/serviceWorker.ts
@@ -2,6 +2,7 @@ export type {};
 declare const self: ServiceWorkerGlobalScope;
 
 import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+import { clientsClaim } from 'workbox-core'
 import { NotificationMessage, CustomData } from './swTypes';
 
 
@@ -48,3 +49,6 @@ self.addEventListener(
     }, 
     false
 );
+
+self.skipWaiting().catch(e => console.error(e));
+clientsClaim()
